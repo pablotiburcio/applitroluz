464002ddfb999352712af0aa911247523692fcd2
Marcos Luz <marcos.antonioluz36@gmail.com>
2021-08-28 17:49:53 -0300

    FEAT: lista e retorna o caminho relativo das imagens utilizadas

---
+++ src/utils/public_assets.ts
@@ -0,0 +1,38 @@
+const GUIDE_ICON = [
+  require('../../assets/guide/lampiao.png'),
+  require('../../assets/guide/iluminacaoInterna.png'),
+  require('../../assets/guide/materiais.png'),
+  require('../../assets/guide/poste.png'),
+];
+  
+const LAMPIAO_STEP_IMAGE = [
+  require('../../assets/guide/lampiao/step00.png'),
+  require('../../assets/guide/lampiao/step01.png'),
+  require('../../assets/guide/lampiao/step02.png'),
+  require('../../assets/guide/lampiao/step03.png'),
+  require('../../assets/guide/lampiao/step04.png'),
+  require('../../assets/guide/lampiao/step05.png'),
+  require('../../assets/guide/lampiao/step06.png'),
+  require('../../assets/guide/lampiao/step07.png'),
+  require('../../assets/guide/lampiao/step08.png'),
+  require('../../assets/guide/lampiao/step09.png'),
+  require('../../assets/guide/lampiao/step10.png'),
+];
+
+const POSTE_STEP_IMAGE = [
+  require('../../assets/guide/poste/step01.png'),
+  require('../../assets/guide/poste/step02.png'),
+  require('../../assets/guide/poste/step03.png'),
+  require('../../assets/guide/poste/step04.png'),
+  require('../../assets/guide/poste/step05.png'),
+  require('../../assets/guide/poste/step06.png'),
+  require('../../assets/guide/poste/step07.png'),
+  require('../../assets/guide/poste/step08.png'),
+  require('../../assets/guide/poste/step09.png'),
+];
+  
+export { 
+  GUIDE_ICON, 
+  LAMPIAO_STEP_IMAGE, 
+  POSTE_STEP_IMAGE 
+};
